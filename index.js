5c5d599a3dd6b957cc77026d2c382f313f385d73
Emme Porter DeWitt <empdewitt@gmail.com>
2018-01-13 20:38:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
-function happyHolidays(string) {
-  console.log("Happy holidays!")
+function happyHolidays() {
+  return ("Happy holidays!")
 }
 function happyHolidaysTo(name) {
   console.log('Happy holidays, ${name}!')
